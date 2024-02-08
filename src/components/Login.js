39f809725cd55d8f663dbb472dd9b599e72c7fd8
Nitin Dagadu Patil <113434416+nitindpatil15@users.noreply.github.com>
2024-02-08 22:20:31 +0530

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -10,7 +10,7 @@ const Login = (props) => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch(`http://localhost:5000/api/auth/login/`, {
+      const response = await fetch(`https://inotebook-backend-jade.vercel.app/api/auth/login/`, {
         method: "POST",
         headers: {
           "content-type": "application/json",
