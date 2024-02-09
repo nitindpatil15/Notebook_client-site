84ef018993fd7bc7e1f858812944284bf18e8551
nitindpatil15 <nitindpatil2002@gmail.com>
2024-02-09 15:45:50 +0530

    Link changing

--- src/components/Login.js
+++ src/components/Login.js
@@ -10,7 +10,7 @@ const Login = (props) => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch(`http://localhost:5000/api/auth/login/`, {
+      const response = await fetch(`https://inotebookserver-8bgsn8l58-nitin-patils-projects.vercel.app/api/auth/login/`, {
         method: "POST",
         headers: {
           "content-type": "application/json",
